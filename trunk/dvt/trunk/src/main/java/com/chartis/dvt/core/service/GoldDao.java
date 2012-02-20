16986a56219fdebfb71ad4f7d808a8e64d8b882e
chaminda.sl@gmail.com <chaminda.sl@gmail.com@12554e77-58f1-ed2e-7f3e-83e73ce6fb53>
2012-02-20 16:28:58 +0000

    [dvt] added GoldDao and Impl

---
+++ trunk/dvt/trunk/src/main/java/com/chartis/dvt/core/service/GoldDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chartis.dvt.core.service;
+
+import com.chartis.dvt.core.model.PolicyKeys;
+
+/**
+ *
+ * @author chams
+ */
+public interface GoldDao {
+    Integer getMajorLine(final PolicyKeys policyKeys);
+}
