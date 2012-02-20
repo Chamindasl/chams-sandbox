94821dc80a4b2e36cb23537fb8b9002a43cb3aed
chaminda.sl@gmail.com <chaminda.sl@gmail.com@12554e77-58f1-ed2e-7f3e-83e73ce6fb53>
2012-02-20 16:40:54 +0000

    [dvt] to avoid the break

--- trunk/dvt/trunk/src/main/java/com/chartis/dvt/core/service/impl/GoldDaoImpl.java
+++ trunk/dvt/trunk/src/main/java/com/chartis/dvt/core/service/impl/GoldDaoImpl.java
@@ -17,30 +17,31 @@ public class GoldDaoImpl implements GoldDao {
     private DataSource dataSource;
 
     public Integer getMajorLine(PolicyKeys policyKeys) {
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            con = dataSource.getConnection();
-            ps = con.prepareStatement("select * from dvt_columns " + "where table_name = ? order by lob_name");
-            ps.setString(1, lob);
-            rs = ps.executeQuery();
-            DvtTable dvtTable = null;
-            while (rs.next()) {
-                final DvtColumn column = new RowMapper().map(rs);
-                if (dvtTable == null) {
-                    dvtTable = column.getDvtTable();
-                } else {
-                    column.setDvtTable(dvtTable);
-                    dvtTable.addDvtColumn(column);
-                }
-
-            }
-            return dvtColumns;
-        } finally {
-            rs.close();
-            ps.close();
-            con.close();
-        }
+//        Connection con = null;
+//        PreparedStatement ps = null;
+//        ResultSet rs = null;
+//        try {
+//            con = dataSource.getConnection();
+//            ps = con.prepareStatement("select * from dvt_columns " + "where table_name = ? order by lob_name");
+//            ps.setString(1, lob);
+//            rs = ps.executeQuery();
+//            DvtTable dvtTable = null;
+//            while (rs.next()) {
+//                final DvtColumn column = new RowMapper().map(rs);
+//                if (dvtTable == null) {
+//                    dvtTable = column.getDvtTable();
+//                } else {
+//                    column.setDvtTable(dvtTable);
+//                    dvtTable.addDvtColumn(column);
+//                }
+//
+//            }
+//            return dvtColumns;
+//        } finally {
+//            rs.close();
+//            ps.close();
+//            con.close();
+//        }
+        return 1;
     }
 }
