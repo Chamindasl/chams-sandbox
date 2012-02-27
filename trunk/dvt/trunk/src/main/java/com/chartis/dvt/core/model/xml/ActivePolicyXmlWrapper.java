653f5217ee7fa438ba143c55361329929fa014f7
chaminda.sl@gmail.com <chaminda.sl@gmail.com@12554e77-58f1-ed2e-7f3e-83e73ce6fb53>
2012-02-27 02:06:34 +0000

    [dvt] xpath settings in a properties

--- trunk/dvt/trunk/src/main/java/com/chartis/dvt/core/model/xml/ActivePolicyXmlWrapper.java
+++ trunk/dvt/trunk/src/main/java/com/chartis/dvt/core/model/xml/ActivePolicyXmlWrapper.java
@@ -3,6 +3,7 @@ package com.chartis.dvt.core.model.xml;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import com.chartis.dvt.core.model.PolicyKeys;
 import com.chartis.dvt.core.model.exception.DvtException;
@@ -19,11 +20,13 @@ import org.w3c.dom.Document;
 
 public class ActivePolicyXmlWrapper {
 
-    public static String XP_POLICY_OFFICE_CD = "//GDSTransmissionRq/BookingPolicy/PolOfficeCd";
-    public static String XP_POLICY_NO = "//GDSTransmissionRq/BookingPolicy/PolicyNo";
-    public static String XP_POLICY_CERTIFICATE_NO = "//GDSTransmissionRq/BookingPolicy/CertificateNo";
-    public static String XP_POLICY_RENL_CERT_NO = "//GDSTransmissionRq/BookingPolicy/RenlCertNo";
-    public static String XP_POLICY_EFF_DT_SEQ_NO = "//GDSTransmissionRq/BookingPolicy/EffDtSeqNo";
+    private final static ResourceBundle BUNDLE = java.util.ResourceBundle.getBundle("xpath");
+    public final static String XP_POLICY_OFFICE_CD = BUNDLE.getString("XP_POLICY_OFFICE_CD");
+    public final static String XP_POLICY_NO = BUNDLE.getString("XP_POLICY_NO");
+    public final static String XP_POLICY_CERTIFICATE_NO = BUNDLE.getString("XP_POLICY_CERTIFICATE_NO");
+    public final static String XP_POLICY_RENL_CERT_NO = BUNDLE.getString("XP_POLICY_RENL_CERT_NO");
+    public final static String XP_POLICY_EFF_DT_SEQ_NO = BUNDLE.getString("XP_POLICY_EFF_DT_SEQ_NO");
+    public final static String XP_SOURCE_SYSTEM_ID = BUNDLE.getString("XP_SOURCE_SYSTEM_ID");
 
     private Document document;
     private XPath xpath;
@@ -51,16 +54,11 @@ public class ActivePolicyXmlWrapper {
 
     private void loadPolicyKeys() throws XPathExpressionException {
         policyKeys = new PolicyKeys();
-//        policyKeys.setPolOfficeCd((String) xpath.compile(XP_POLICY_OFFICE_CD).evaluate(document, XPathConstants.STRING));
-//        policyKeys.setPolicyNo((String) xpath.compile(XP_POLICY_NO).evaluate(document, XPathConstants.STRING));
-//        policyKeys.setCertificateNo(((Double) xpath.compile(XP_POLICY_CERTIFICATE_NO).evaluate(document, XPathConstants.NUMBER)).intValue());
-//        policyKeys.setRenlCertNo((String) xpath.compile(XP_POLICY_RENL_CERT_NO).evaluate(document, XPathConstants.STRING));
-//        policyKeys.setEffDtSeqNo(((Double) xpath.compile(XP_POLICY_EFF_DT_SEQ_NO).evaluate(document, XPathConstants.NUMBER)).intValue());
-        policyKeys.setPolOfficeCd((String) xpath.compile("//pol_office_cd").evaluate(document, XPathConstants.STRING));
-        policyKeys.setPolicyNo((String) xpath.compile("//policy_no").evaluate(document, XPathConstants.STRING));
-        policyKeys.setCertificateNo(((Double) xpath.compile("//certificate_no").evaluate(document, XPathConstants.NUMBER)).intValue());
-        policyKeys.setRenlCertNo((String) xpath.compile("//renl_cert_no").evaluate(document, XPathConstants.STRING));
-        policyKeys.setEffDtSeqNo(((Double) xpath.compile("//eff_dt_seq_no").evaluate(document, XPathConstants.NUMBER)).intValue());
+        policyKeys.setPolOfficeCd((String) xpath.compile(XP_POLICY_OFFICE_CD).evaluate(document, XPathConstants.STRING));
+        policyKeys.setPolicyNo((String) xpath.compile(XP_POLICY_NO).evaluate(document, XPathConstants.STRING));
+        policyKeys.setCertificateNo(((Double) xpath.compile(XP_POLICY_CERTIFICATE_NO).evaluate(document, XPathConstants.NUMBER)).intValue());
+        policyKeys.setRenlCertNo((String) xpath.compile(XP_POLICY_RENL_CERT_NO).evaluate(document, XPathConstants.STRING));
+        policyKeys.setEffDtSeqNo(((Double) xpath.compile(XP_POLICY_EFF_DT_SEQ_NO).evaluate(document, XPathConstants.NUMBER)).intValue());
     }
     
     public String getValue(final String xpathName) throws XPathExpressionException {
@@ -74,7 +72,7 @@ public class ActivePolicyXmlWrapper {
     
     private void loadOtherKeys() throws XPathExpressionException {
         final Map <String, String> temp = new HashMap<String, String>();
-        temp.put("SourceSystemId", ((String) xpath.compile("//source_system_id").evaluate(document, XPathConstants.STRING)));
+        temp.put("SourceSystemId", ((String) xpath.compile(XP_SOURCE_SYSTEM_ID).evaluate(document, XPathConstants.STRING)));
         otherKeys = Collections.unmodifiableMap(temp);
     }
     
