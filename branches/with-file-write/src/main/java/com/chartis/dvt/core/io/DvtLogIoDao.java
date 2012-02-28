c350391a2af1d1ec039c9bf79445e74ee05962ea
chaminda.sl@gmail.com <chaminda.sl@gmail.com@12554e77-58f1-ed2e-7f3e-83e73ce6fb53>
2012-02-28 10:51:40 +0000

    [dvt] mkdir with-file-write

--- branches/with-file-write/src/main/java/com/chartis/dvt/core/io/DvtLogIoDao.java
+++
@@ -1,9 +0,0 @@
-package com.chartis.dvt.core.io;
-
-import java.io.Closeable;
-import java.io.Flushable;
-
-import com.chartis.dvt.core.dao.DvtLogDao;
-
-public interface DvtLogIoDao extends DvtLogDao, Closeable, Flushable{
-}
