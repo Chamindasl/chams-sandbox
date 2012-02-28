2f74bb939f6ca40b0a7c319b100d456fb880a77b
chaminda.sl@gmail.com <chaminda.sl@gmail.com@12554e77-58f1-ed2e-7f3e-83e73ce6fb53>
2012-02-28 10:44:52 +0000

    [dvt] with-file-write

---
+++ trunk/dvt/branches/with-file-write/src/test/java/com/chartis/dvt/core/service/impl/GoldDaoImplTest.java
@@ -0,0 +1,38 @@
+package com.chartis.dvt.core.service.impl;
+
+import static org.junit.Assert.*;
+import static com.chartis.dvt.core.test.util.TestData.*;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.chartis.dvt.core.dao.impl.GoldDaoImpl;
+import com.chartis.dvt.core.jdbc.SimpleDataSourceProvider;
+
+/**
+ * 
+ * @author CHAMINDA.AMARASINGHE
+ *
+ */
+public class GoldDaoImplTest {
+
+    private GoldDaoImpl goldDaoImpl;
+    @Before
+    public void setUp() throws Exception {
+        goldDaoImpl = new GoldDaoImpl();
+        goldDaoImpl.setDataSource(new SimpleDataSourceProvider().getDataSource());
+    }
+
+    @Test
+    public final void testGetMajorLine() throws SQLException {
+        assertEquals(2, goldDaoImpl.getMajorLine(samplePolicyKeys()).intValue());
+    }
+
+    @Test
+    public final void testGetRecordAsMap() throws SQLException {
+        assertEquals(27, goldDaoImpl.getRecordAsMap("tpolkey", samplePolicyKeys()).size());
+    }
+
+}
