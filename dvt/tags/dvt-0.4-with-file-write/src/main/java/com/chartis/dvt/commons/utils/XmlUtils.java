03b9b062b6e5bb85ff3d1cdc8b3a93da7d573f68
nda.sl <chaminda.sl@gmail.com>
2012-02-28 11:06:45 +0000

    [maven-release-plugin]  copy for tag dvt-0.4-with-file-write

---
+++ dvt/tags/dvt-0.4-with-file-write/src/main/java/com/chartis/dvt/commons/utils/XmlUtils.java
@@ -0,0 +1,37 @@
+package com.chartis.dvt.commons.utils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * @author CHAMINDA.AMARASINGHE
+ *
+ */
+public class XmlUtils {
+
+    /**
+     * 
+     */
+    private XmlUtils() {
+
+    }
+
+    public static Document fileAsDocument(final File file) throws ParserConfigurationException, SAXException,
+            IOException {
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        final DocumentBuilder builder = factory.newDocumentBuilder();
+        final InputSource is = new InputSource(new FileReader(file));
+        return builder.parse(is);
+
+    }
+}
