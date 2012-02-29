9b8826e58b86f2e2c6f054430607c8f5cfdf87b7
chaminda.sl@gmail.com <chaminda.sl@gmail.com@12554e77-58f1-ed2e-7f3e-83e73ce6fb53>
2012-02-29 03:01:07 +0000

    [dvt] added few tests

--- trunk/dvt/branches/with-file-write/src/test/java/com/chartis/dvt/core/model/xml/ActivePolicyXmlWrapperTest.java
+++ trunk/dvt/branches/with-file-write/src/test/java/com/chartis/dvt/core/model/xml/ActivePolicyXmlWrapperTest.java
@@ -1,4 +1,4 @@
-package com.chartis.dvt.core.xml.model;
+package com.chartis.dvt.core.model.xml;
 
 import javax.xml.xpath.XPathExpressionException;
 
@@ -30,9 +30,24 @@ public class ActivePolicyXmlWrapperTest {
     }
 
     @Test()
-    public void getValue() throws Exception{
+    public void testGetValue() throws Exception{
         final ActivePolicyXmlWrapper wrapper = new ActivePolicyXmlWrapper(TestData.stringToDoc(TestData.L3_DOC));
         assertEquals("Name of Street", wrapper.getValue("/Policy/Client/Address/Street"));
+        
+    }
+
+    @Test()
+    public void testNotExistingGet() throws Exception{
+        final ActivePolicyXmlWrapper wrapper = new ActivePolicyXmlWrapper(TestData.stringToDoc(TestData.L3_DOC));
+        assertEquals("", wrapper.getValue("/Policy/Client/Address/Street/NOT_EXIST"));
+        
+    }
+
+    @Test()
+    public void testLoadOtherKeys() throws Exception{
+        final ActivePolicyXmlWrapper wrapper = new ActivePolicyXmlWrapper(TestData.stringToDoc(TestData.L3_DOC));
+        assertEquals("01", wrapper.getOtherKeys().get(ActivePolicyXmlWrapper.SOURCE_SYSTEM_ID));
+        assertEquals("E", wrapper.getOtherKeys().get(ActivePolicyXmlWrapper.RECORD_TYPE_CD));
         
     }
 
