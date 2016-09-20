5e55e7f3ef633c28ea6a8bfbf4650ef093ef986c
Chaminda Amarasinghe <chams@Chamindas-MacBook-Pro.local>
2016-09-20 08:54:57 +0100

    fixed spelling

--- tricky-words/app/TrickyWordCtrl.js
+++ tricky-words/app/TrickyWordCtrl.js
@@ -27,7 +27,7 @@ function TrickyWordCtrl($scope, $timeout) {
 
     $scope.fillTw = function () {
 
-        $scope.text = "Max is at the park Max's dad is at the park Kitty is at the park too I can race you to the swings says Max Max and Kitty run to the swings Dad run too Max gets to the swings Look I am at the swrings says Max Kitty and Max get onto the swings Dad can not get onto the swings He is too big Can you push me syas Max Can you push me says Kitty Dad pushes Kitty and Max They are going up Max sees a red slide I can race you to the red slide says Max Max and Kitty run to the red slide Dad runs too Kitty gets to the red slide Look I am at the red slide says Ktty Kitty and Max get onto the slide Dad cannot get onto the slide He is too big Kitty is going down Max is going down too Look out Kitty says Max Here I am I can rece you to the see-saw says Kitty Kitty and Max run to the see-saw Dan runs too Ktty jumps onto the see-saw Max jumps onto the see-saw too Dan cannot get onto the see-saw He is too big Kitty is going up on the see-saw Max is going down on the see-saw They are going up and down on the see-saw Max sees a little horse He jumps down and runs to the little horse Look out kitty says dad Kitty and Dan run to Max Max is on the little horse Kitty gets onto the little horse too Dad cannot get onto the littel horse He is too big Dad pushes the little horse This is fun says Kitty This is fun says Max Kitty sees a big horse Look says Kitty I can see a big horse Kitty runs to the big horse Max sees the big horse and run to it Dad runs to the big horse too can I pet the big horse say Kitty Can I pet the big horse says Max Yes can pet the big horse says Dad Max and Kitty cannot pet the big horse The horse is too big The big horse looks down at kitty and Max They look up at the big horse They cannot pet the big horse Dan can pet the big horse The horse is big and Dad is big Up up says Dad The horse is big and Dad is big says Max I the he she me we be was to do are all you your come some said here there they go no so my one by like have live give";
+        $scope.text = "Max is at the park Max's dad is at the park Kitty is at the park too I can race you to the swings says Max Max and Kitty run to the swings Dad run too Max gets to the swings Look I am at the swrings says Max Kitty and Max get onto the swings Dad can not get onto the swings He is too big Can you push me says Max Can you push me says Kitty Dad pushes Kitty and Max They are going up Max sees a red slide I can race you to the red slide says Max Max and Kitty run to the red slide Dad runs too Kitty gets to the red slide Look I am at the red slide says Kitty Kitty and Max get onto the slide Dad cannot get onto the slide He is too big Kitty is going down Max is going down too Look out Kitty says Max Here I am I can race you to the see-saw says Kitty Kitty and Max run to the see-saw Dad runs too Kitty jumps onto the see-saw Max jumps onto the see-saw too Dad cannot get onto the see-saw He is too big Kitty is going up on the see-saw Max is going down on the see-saw They are going up and down on the see-saw Max sees a little horse He jumps down and runs to the little horse Look out kitty says dad Kitty and Dad run to Max Max is on the little horse Kitty gets onto the little horse too Dad cannot get onto the little horse He is too big Dad pushes the little horse This is fun says Kitty This is fun says Max Kitty sees a big horse Look says Kitty I can see a big horse Kitty runs to the big horse Max sees the big horse and run to it Dad runs to the big horse too can I pet the big horse say Kitty Can I pet the big horse says Max Yes can pet the big horse says Dad Max and Kitty cannot pet the big horse The horse is too big The big horse looks down at kitty and Max They look up at the big horse They cannot pet the big horse Dad can pet the big horse The horse is big and Dad is big Up up says Dad The horse is big and Dad is big says Max";
 //Max is at the park.\
 //Max's dad is at the park.\
 // Kitty is at the park too.\
@@ -39,7 +39,7 @@ function TrickyWordCtrl($scope, $timeout) {
 // Kitty and Max get onto the swings.\
 // Dad can not get onto the swings.\
 // He is too big.\
-// Can you push me syas Max.\
+// Can you push me says Max.\
 // Can you push me says Kitty.\
 // Dad pushes Kitty and Max.\
 // They are going up.\
@@ -48,7 +48,7 @@ function TrickyWordCtrl($scope, $timeout) {
 // Max and Kitty run to the red slide.\
 // Dad runs too.\
 // Kitty gets to the red slide.\
-// Look I am at the red slide says Ktty.\
+// Look I am at the red slide says Kitty.\
 // Kitty and Max get onto the slide.\
 // Dad cannot get onto the slide.\
 // He is too big.\
@@ -56,12 +56,12 @@ function TrickyWordCtrl($scope, $timeout) {
 // Max is going down too.\
 // Look out Kitty says Max.\
 // Here I am.\
-// I can rece you to the see-saw says Kitty.\
+// I can race you to the see-saw says Kitty.\
 // Kitty and Max run to the see-saw.\
-// Dan runs too.\
-// Ktty jumps onto the see-saw.\
+// Dad runs too.\
+// Kitty jumps onto the see-saw.\
 // Max jumps onto the see-saw too.\
-// Dan cannot get onto the see-saw.\
+// Dad cannot get onto the see-saw.\
 // He is too big.\
 // Kitty is going up on the see-saw.\
 // Max is going down on the see-saw.\
@@ -69,7 +69,7 @@ function TrickyWordCtrl($scope, $timeout) {
 // Max sees a little horse.\
 // He jumps down and runs to the little horse.\
 // Look out kitty says dad.\
-// Kitty and Dan run to Max.\
+// Kitty and Dad run to Max.\
 // Max is on the little horse.\
 // Kitty gets onto the little horse too.\
 // Dad cannot get onto the littel horse.\
@@ -91,7 +91,7 @@ function TrickyWordCtrl($scope, $timeout) {
 // The big horse looks down at kitty and Max.\
 // They look up at the big horse.\
 // They cannot pet the big horse.\
-// Dan can pet the big horse.\
+// Dad can pet the big horse.\
 // The horse is big and Dad is big.\
 // Up up says Dad.\
 // The horse is big and Dad is big says Max.\
